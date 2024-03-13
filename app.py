cf23f1bbb45ef5b206bfd0eda4569d5367619d8f
mooohh <mohamedettadrarty@gmail.com>
2024-03-13 18:17:37 +0100

    resolve the streamlit database problem

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 # pylint: disable=missing-module-docstring
 import ast
 import io
-
+import  os
+import logging
 import duckdb
 import streamlit as st
+
+
+if "data" not in os.listdir():
+    logging.error(os.listdir())
+    logging.error("create the data folder")
+    os.mkdir("data")
+
+if 'exercises_sql_tables.duckdb' not in os.listdir("data"):
+    exec(open("init_db.py").read())
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
@@ -14,7 +24,7 @@ with st.sidebar:
         index=None,
         placeholder="Select contact method...",
     )
-    exercise_df = con.execute(f"SELECT * FROM exercises_list_df WHERE theme='{theme}'").df()
+    exercise_df = con.execute(f"SELECT * FROM exercises_list_df WHERE theme='{theme}'").df().sort_values(by='last_reviewed', ascending=True ).reset_index()
 
     st.write("You selected : ", exercise_df)
 
@@ -26,35 +36,37 @@ st.write(
 )
 
 request = st.text_area(label="Votre code SQL ici")
-TRUE_REQUEST = "SELECT * FROM beverages"
 
-if request != "":
-    user_answer_df = con.execute(query=request) .df()
-    st.write(user_answer_df)
-    real_answer_df = con.execute(TRUE_REQUEST).df()
-    try:
-        comparison_result = user_answer_df.compare(real_answer_df[user_answer_df.columns])
-        if comparison_result.empty:
-            st.write("You Succeed")
-        else:
-            st.write("You did not succeed")
-            st.write(comparison_result)
-    except ValueError as e:
-        st.write("the result is not identical")
-else:
-    st.write("Your should write a request")
-
-tables_tab, solution_tab = st.tabs(["Tables", "Solution"])
-
-with tables_tab:
-    exercise_tables = ast.literal_eval(exercise_df.loc[0, 'tables'])
-    for table in exercise_tables:
-        table_df = con.execute(f"SELECT * FROM {table}").df()
-        st.write(f"{table}", table_df)
-
-with solution_tab:
+try:
     ANSWER_FILE = exercise_df.loc[0, "exercises_name"]
     with open(f"answers/{ANSWER_FILE}.sql", "r") as f:
         ANSWER_STR = f.read()
 
-    st.write(ANSWER_STR)
+    if request != "":
+        user_answer_df = con.execute(query=request).df()
+        st.write(user_answer_df)
+        real_answer_df = con.execute(ANSWER_STR).df()
+        try:
+            comparison_result = user_answer_df.compare(real_answer_df[user_answer_df.columns])
+            if comparison_result.empty:
+                st.write("You Succeed")
+            else:
+                st.write("You did not succeed")
+                st.write(comparison_result)
+        except (ValueError, KeyError) as e:
+            st.write("the result is not identical")
+    else:
+        st.write("Your should write a request")
+
+    tables_tab, solution_tab = st.tabs(["Tables", "Solution"])
+    with tables_tab:
+        exercise_tables = exercise_df.loc[0, 'tables']
+        for table in exercise_tables:
+            table_df = con.execute(f"SELECT * FROM {table}").df()
+            st.write(f"{table}", table_df)
+
+    with solution_tab:
+        st.write(ANSWER_STR)
+except (ValueError, KeyError):
+    st.write("You should choose an exercise please ")
+
--- init_db.py
+++ init_db.py
@@ -9,11 +9,11 @@ con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=Fals
 # --------------------------------------------------------------------------
 
 exercises_list = {
-    "theme": ["cross_joins", "window_functions"],
-    "exercises_name": ["beverages_and_food", "simple_window"],
-    "tables": [["beverages", "food_items"], "simple_window"],
-    "last_reviewed": ["1970-01-01", "1970-01-01"],
-}
+                  "theme": ["cross_joins", "cross_joins","cross_joins","cross_joins"],
+                  "exercises_name": ["beverages_and_food", "sizes_and_trademarks","s","s"],
+                  "tables": [["beverages", "food_items"], ["sizes", "trademarks"], ["s"],["s"]],
+                  "last_reviewed": ["1990-01-01", "1970-01-01", "1980-01-01" , "2000-01-01"],
+                  }
 
 exercises_list_df = pd.DataFrame(exercises_list)
 con.execute("CREATE TABLE IF NOT EXISTS exercises_list_df AS SELECT * FROM exercises_list_df")
@@ -21,6 +21,8 @@ con.execute("CREATE TABLE IF NOT EXISTS exercises_list_df AS SELECT * FROM exerc
 # ------------------------------------------------------------------------
 # CROSS JOIN EXERCISES
 # ------------------------------------------------------------------------
+
+# FIRST EXERCISE
 
 CSV = """
 beverage,price
@@ -41,3 +43,28 @@ muffin,3
 
 food_items = pd.read_csv(io.StringIO(CSV2))
 con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
+
+# SECOND EXERCISE
+CSV3 = '''
+size
+XS
+M
+L
+XL
+'''
+
+sizes = pd.read_csv(io.StringIO(CSV3))
+con.execute("CREATE TABLE IF NOT EXISTS sizes AS SELECT * FROM sizes")
+
+CSV4 = '''
+trademark
+Nike
+Asphalte
+Abercrombie
+Lewis
+'''
+
+trademarks = pd.read_csv(io.StringIO(CSV4))
+con.execute("CREATE TABLE IF NOT EXISTS trademarks AS SELECT * FROM trademarks")
+
+con.close()
